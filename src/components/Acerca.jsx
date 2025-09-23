8282d491e5d02fc07db9c72de81e22fefd14613b
Guille <guibear0@gmail.com>
2025-09-23 19:06:17 +0200

    CvGuille

--- src/components/Acerca.jsx
+++ src/components/Acerca.jsx
@@ -1,4 +1,3 @@
-// src/components/Acerca.jsx
 import "../comp_styles/Acerca.css";
 import {
   FaHtml5,
@@ -24,12 +23,14 @@ function Acerca() {
           </p>
           <h2>Estudios</h2>
           <h3>Formación en CIFP A Carballeira</h3>
-      
-            <p>Graduado en Desarrollo de Aplicaciones Web <br/>            
-            Estudiando Desarrollo de Aplicaciones Multiplataforma</p>
+          <p>
+            Graduado en Desarrollo de Aplicaciones Web <br/>            
+            Estudiando Desarrollo de Aplicaciones Multiplataforma
+          </p>
           <h2>Certificados</h2>
-            <p>Ciberseguridad Avanzada en Entornos de <br/>las Tecnologías de la Operación</p>
+          <p>Ciberseguridad Avanzada en Entornos de <br/>las Tecnologías de la Operación</p>
         </div>
+
         <div className="bloque-tecnologias">
           <h2>Tecnologías</h2>
           <h3>Uso habitual</h3>
@@ -43,6 +44,21 @@ function Acerca() {
             <RiSupabaseFill className="iconos supabase" title="Supabase"/>
             <FaWordpress className="iconos wordpress" title="WordPress" />
             <SiLaravel className="iconos laravel" title="Laravel"/>
+          </div>
+
+          {/* Bloque de CV */}
+          <div className="bloque-cv">
+            <h2>Mi CV</h2>
+            <h3>Descarga mi CV si lo prefieres</h3>
+            <div className="cv-contenido">
+              <img 
+                src="https://via.placeholder.com/150x200.png?text=PDF+Preview" 
+                alt="Vista previa CV" 
+              />
+              <a href="assets/CV_Guillermo_Bello_Armada.pdf" download className="btn-cv">
+                Descargar PDF
+              </a>
+            </div>
           </div>
         </div>
       </div>
