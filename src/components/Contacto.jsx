cae0da92f5f9f86411a9c2f7f302196b1e5b84eb
Guille <guibear0@gmail.com>
2025-09-23 18:48:55 +0200

    WhileInViewContacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -63,7 +63,7 @@ function Contacto() {
     <motion.section
       id="contacto"
       initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
+      whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
     >
       <h2>Contacto</h2>
